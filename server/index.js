0e6b645346513d01831f17d33368c1cd9bb8f1a5
aditya <abhilashaditya77@gmail.com>
2024-08-05 02:47:56 +0530

    changes made 1

--- server/index.js
+++ server/index.js
@@ -9,10 +9,29 @@ Connection()
 
 const app = express()
 
+
 //middlewares
 app.use(express.json()) //convert to json format
 app.use(cookieParser()); 
-app.use(cors({withCredentials: true, origin:"https://www.algoapex.online"}));
+// app.use(cors({withCredentials: true, origin:"https://www.algoapex.online"}));
+
+const allowedOrigins = [
+    'https://www.algoapex.online',
+    'http://localhost:5173',
+    'https://online-judge-frkkz97yy-aditya-abhilashs-projects.vercel.app'
+];
+
+app.use(cors({
+    withCredentials: true,
+    origin: function(origin, callback){
+      if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    }
+  }));
+
 app.use(express.urlencoded({ extended: true }));
 
 
