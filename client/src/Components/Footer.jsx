4c4e3b0624802e9f69e3211b52381709a9b51cbd
aditya <abhilashaditya77@gmail.com>
2024-08-05 21:38:46 +0530

    footer changed

--- client/src/Components/Footer.jsx
+++ client/src/Components/Footer.jsx
@@ -2,20 +2,19 @@ import React from 'react';
 import '../assets/css/footer.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
-
 const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer-content">
         <p>Created by Aditya Abhilash</p>
         <div className="social-links">
-          <a href="https://www.instagram.com/adityaabhilash1" target="_blank" rel="noopener noreferrer">
+          <a href="https://www.instagram.com/adityaabhilash1" target="_blank" rel="noopener noreferrer" aria-label="Instagram">
             <i className="fab fa-instagram"></i>
           </a>
-          <a href="https://www.linkedin.com/in/aditya-abhilash-6912bb291/" target="_blank" rel="noopener noreferrer">
+          <a href="https://www.linkedin.com/in/aditya-abhilash-6912bb291/" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn">
             <i className="fab fa-linkedin"></i>
           </a>
-          <a href="https://github.com/AdityaAbhilash" target="_blank" rel="noopener noreferrer">
+          <a href="https://github.com/AdityaAbhilash" target="_blank" rel="noopener noreferrer" aria-label="GitHub">
             <i className="fab fa-github"></i>
           </a>
         </div>
